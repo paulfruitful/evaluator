9ab80cafe85b10d03369f30ad0288d891e980247
Paul Fruitful <fruitful2007@outlook.com>
2024-08-26 22:57:01 +0100

    Navv

--- app/_components/Nav.jsx
+++ app/_components/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Nav = () => {
+const Nav = ({josefin,mulish}) => {
   return (
-    <div className='flex w-full bg-transparent'>
-      
+    <div className='flex w-full justify-between bg-transparent'>
+      <div className="flex self-center items-center p-6">
+        <img src="/logo.png" className='w-[60px] lg:w-[80px] lg:h-[80px] p-3 h-[60px]' alt="" />
+        <span className={`text-3xl lg:text-[2.7rem] pt-3 lg:pt-4 text-center ${josefin.className} font-bold text-white`}>Evaluator</span>
+      </div>
+
+      <div className="justify-self-end items-center self-center p-3 lg:p-6">
+        <a href="" className={`bg-white self-center rounded-lg ${mulish.className} p-3 text-lg font-semibold mx-1 text-indigo-600`}>Login</a>
+        <a href="" className={`bg-transparent self-center rounded-lg ${mulish.className} p-3 text-lg font-semibold border-2 border-white border-solid shadow-lg text-white`}>Sign Up</a>
+
+      </div>
     </div>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Josefin_Sans,Mulish } from "next/font/google";
+import Nav from "./_components/Nav";
+
 
 const inter = Inter({ subsets: ["latin"] });
-
+const josefin=Josefin_Sans({subsets:['latin'],weight:['100','200','300','400','500','600']})
+const mulish=Mulish({subsets:['latin'],weight:['200','300','400','500','600']})
 export const metadata = {
   title: "Evaluator",
   description: "Rate your instructors,teachers, staffs and get recommendations",
@@ -11,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-indigo-600 ${inter.className}`}>
+        <Nav josefin={josefin} mulish={mulish}/>
+        {children}
+        </body>
     </html>
   );
 }
