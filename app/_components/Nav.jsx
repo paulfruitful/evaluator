d1a0ca82472c716eba37a896407860d555bf94f9
Paul Fruitful <fruitful2007@outlook.com>
2024-08-26 23:14:49 +0100

    Nav

--- app/_components/Nav.jsx
+++ app/_components/Nav.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const Nav = ({josefin,mulish}) => {
+const Nav = ({ josefin, mulish }) => {
   return (
-    <div className='flex w-full justify-between bg-transparent'>
-      <div className="flex self-center items-center p-6">
-        <img src="/logo.png" className='w-[60px] lg:w-[80px] lg:h-[80px] p-3 h-[60px]' alt="" />
-        <span className={`text-3xl lg:text-[2.7rem] pt-3 lg:pt-4 text-center ${josefin.className} font-bold text-white`}>Evaluator</span>
+    <div className="flex w-full justify-between bg-transparent">
+      <div className="flex items-center p-6">
+        <img
+          src="/logo.png"
+          className="w-[60px] lg:w-[80px] lg:h-[80px] p-3 h-[60px]"
+          alt="Logo"
+        />
+        <span
+          className={`text-3xl lg:text-[2.7rem] pt-3 lg:pt-4 text-center ${josefin.className} font-bold text-white`}
+        >
+          Evaluator
+        </span>
       </div>
 
-      <div className="justify-self-end items-center self-center p-3 lg:p-6">
-        <a href="" className={`bg-white self-center rounded-lg ${mulish.className} p-3 text-lg font-semibold mx-1 text-indigo-600`}>Login</a>
-        <a href="" className={`bg-transparent self-center rounded-lg ${mulish.className} p-3 text-lg font-semibold border-2 border-white border-solid shadow-lg text-white`}>Sign Up</a>
+      <div className="flex items-center p-3 lg:p-6 lg:hidden">
+        <button className="text-white text-2xl">
+          <i className="fas fa-bars"></i> 
+        </button>
+      </div>
 
+      <div className="hidden lg:flex items-center space-x-2 self-center p-3 lg:p-6">
+        <a
+          href="#"
+          className={`bg-white rounded-lg ${mulish.className} p-2 lg:p-3 text-sm lg:text-lg font-semibold text-indigo-600`}
+        >
+          Login
+        </a>
+        <a
+          href="#"
+          className={`bg-transparent rounded-lg ${mulish.className} p-2 lg:p-3 text-sm lg:text-lg font-semibold border-2 border-white border-solid flex shadow-lg text-white`}
+        >
+          <span>Sign Up</span>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
